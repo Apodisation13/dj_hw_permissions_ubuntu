3e54b00812e2828a8c5d8158dcb066146f47a76e
Pavel Soloshenko <apodisation13@gmail.com>
2021-07-31 14:36:34 +0300

    init

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import redirect
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+
+def home(request):
+    return redirect('api/')
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+        return []
